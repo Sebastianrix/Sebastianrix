2f68eadbf1d87c3e93ebad739adfc649be449891
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2021-10-04 11:17:57 +0100

    Update Stalk_v2.java

--- Stalk_v2.java
+++ Stalk_v2.java
@@ -2,84 +2,133 @@ import java.security.spec.EncodedKeySpec;
 import java.util.concurrent.ThreadLocalRandom;
 
 
-class Stalk {
+class Stalk_v2 {
     public static void main(String[] args) {
-      
-      
-      
-      
+
+
+
+
         System.out.println("Stalk"); 
-    
+
 //--------------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------------
+int min1 = -25;
+int max1 =  9;
+
+
+int int_random1 = (int)Math.floor(Math.random()*(max1-min1+1)+min1);
+
+
+
+      System.out.println(int_random1);
+
+
+      String question1= (null);
+if (int_random1 == 9)   {question1 = "9";}else
+if (int_random1 == 8)   {question1 = "8";}else
+if (int_random1 == 7)   {question1 = "7";}else
+if (int_random1 == 6)   {question1 = "6";}else
+if (int_random1 == 5)   {question1 = "5";}else
+if (int_random1 == 4)   {question1 = "4";}else
+if (int_random1 == 3)   {question1 = "3";}else
+if (int_random1 == 2)   {question1 = "2";}else
+if (int_random1 == 1)   {question1 = "1";}else
+if (int_random1 == 0)   {question1 = "0";}else
+if (int_random1 == -1)  {question1 = "a";}else
+if (int_random1 == -2)  {question1 = "b";}else
+if (int_random1 == -3)  {question1 = "c";}else
+if (int_random1 == -4)  {question1 = "d";}else
+if (int_random1 == -5)  {question1 = "e";}else
+if (int_random1 == -6)  {question1 = "f";}else
+if (int_random1 == -7)  {question1 = "g";}else
+if (int_random1 == -8)  {question1 = "h";}else
+if (int_random1 == -9)  {question1 = "i";}else
+if (int_random1 == -10) {question1 = "j";}else
+if (int_random1 == -11) {question1 = "k";}else
+if (int_random1 == -12) {question1 = "l";}else
+if (int_random1 == -13) {question1 = "m";}else
+if (int_random1 == -14) {question1 = "n";}else
+if (int_random1 == -15) {question1 = "o";}else
+if (int_random1 == -16) {question1 = "p";}else
+if (int_random1 == -17) {question1 = "q";}else
+if (int_random1 == -18) {question1 = "r";}else
+if (int_random1 == -19) {question1 = "s";}else
+if (int_random1 == -20) {question1 = "t";}else
+if (int_random1 == -21) {question1 = "u";}else
+if (int_random1 == -22) {question1 = "v";}else
+if (int_random1 == -23) {question1 = "w";}else
+if (int_random1 == -24) {question1 = "x";}else
+if (int_random1 == -25) {question1 = "y";}
+
+System.out.println(question1);
 //--------------------------------------------------------------------------------------------------
 int min2 = -25;
 int max2 =  9;
- 
+
 
 int int_random2 = (int)Math.floor(Math.random()*(max2-min2+1)+min2);
- 
 
-   
-      System.out.println(int_random3);
-     
-      
+
+
+      System.out.println(int_random2);
+
+
       String question2= (null);
-if (int_random3 == 9)   {question3 = "9";}else
-if (int_random3 == 8)   {question3 = "8";}else
-if (int_random3 == 7)   {question3 = "7";}else
-if (int_random3 == 6)   {question3 = "6";}else
-if (int_random3 == 5)   {question3 = "5";}else
-if (int_random3 == 4)   {question3 = "4";}else
-if (int_random3 == 3)   {question3 = "3";}else
-if (int_random3 == 2)   {question3 = "2";}else
-if (int_random3 == 1)   {question3 = "1";}else
-if (int_random3 == 0)   {question3 = "0";}else
-if (int_random3 == -1)  {question3 = "a";}else
-if (int_random3 == -2)  {question3 = "b";}else
-if (int_random3 == -3)  {question3 = "c";}else
-if (int_random3 == -4)  {question3 = "d";}else
-if (int_random3 == -5)  {question3 = "e";}else
-if (int_random3 == -6)  {question3 = "f";}else
-if (int_random3 == -7)  {question3 = "g";}else
-if (int_random3 == -8)  {question3 = "h";}else
-if (int_random3 == -9)  {question3 = "i";}else
-if (int_random3 == -10) {question3 = "j";}else
-if (int_random3 == -11) {question3 = "k";}else
-if (int_random3 == -12) {question3 = "l";}else
-if (int_random3 == -13) {question3 = "m";}else
-if (int_random3 == -14) {question3 = "n";}else
-if (int_random3 == -15) {question3 = "o";}else
-if (int_random3 == -16) {question3 = "p";}else
-if (int_random3 == -17) {question3 = "q";}else
-if (int_random3 == -18) {question3 = "r";}else
-if (int_random3 == -19) {question3 = "s";}else
-if (int_random3 == -20) {question3 = "t";}else
-if (int_random3 == -21) {question3 = "u";}else
-if (int_random3 == -22) {question3 = "v";}else
-if (int_random3 == -23) {question3 = "w";}else
-if (int_random3 == -24) {question3 = "x";}else
-if (int_random3 == -25) {question3 = "y";}
+if (int_random2 == 9)   {question2 = "9";}else
+if (int_random2 == 8)   {question2 = "8";}else
+if (int_random2 == 7)   {question2 = "7";}else
+if (int_random2 == 6)   {question2 = "6";}else
+if (int_random2 == 5)   {question2 = "5";}else
+if (int_random2 == 4)   {question2 = "4";}else
+if (int_random2 == 3)   {question2 = "3";}else
+if (int_random2 == 2)   {question2 = "2";}else
+if (int_random2 == 1)   {question2 = "1";}else
+if (int_random2 == 0)   {question2 = "0";}else
+if (int_random2 == -1)  {question2 = "a";}else
+if (int_random2 == -2)  {question2 = "b";}else
+if (int_random2 == -3)  {question2 = "c";}else
+if (int_random2 == -4)  {question2 = "d";}else
+if (int_random2 == -5)  {question2 = "e";}else
+if (int_random2 == -6)  {question2 = "f";}else
+if (int_random2 == -7)  {question2 = "g";}else
+if (int_random2 == -8)  {question2 = "h";}else
+if (int_random2 == -9)  {question2 = "i";}else
+if (int_random2 == -10) {question2 = "j";}else
+if (int_random2 == -11) {question2 = "k";}else
+if (int_random2 == -12) {question2 = "l";}else
+if (int_random2 == -13) {question2 = "m";}else
+if (int_random2 == -14) {question2 = "n";}else
+if (int_random2 == -15) {question2 = "o";}else
+if (int_random2 == -16) {question2 = "p";}else
+if (int_random2 == -17) {question2 = "q";}else
+if (int_random2 == -18) {question2 = "r";}else
+if (int_random2 == -19) {question2 = "s";}else
+if (int_random2 == -20) {question2 = "t";}else
+if (int_random2 == -21) {question2 = "u";}else
+if (int_random2 == -22) {question2 = "v";}else
+if (int_random2 == -23) {question2 = "w";}else
+if (int_random2 == -24) {question2 = "x";}else
+if (int_random2 == -25) {question2 = "y";}
 
-System.out.println(question3);
-        
-        
+System.out.println(question2);
+
+
 //--------------------------------------------------------------------------------------------------
 
 
 
 int min3 = -25;
 int max3 =  9;
- 
+
 
 int int_random3 = (int)Math.floor(Math.random()*(max3-min3+1)+min3);
- 
 
-   
+
+
       System.out.println(int_random3);
-     
-      
+
+
       String question3= (null);
 if (int_random3 == 9)   {question3 = "9";}else
 if (int_random3 == 8)   {question3 = "8";}else
@@ -126,15 +175,15 @@ System.out.println(question3);
 
 int min4 = -25;
 int max4 =  9;
- 
+
 
 int int_random4 = (int)Math.floor(Math.random()*(max4-min4+1)+min4);
- 
 
-   
+
+
       System.out.println(int_random4);
-     
-      
+
+
       String question4= (null);
 if (int_random4 == 9)   {question4 = "9";}else
 if (int_random4 == 8)   {question4 = "8";}else
@@ -176,15 +225,15 @@ System.out.println(question4);
 //--------------------------------------------------------------------------------------------------
 int min5 = -25;
 int max5 =  9;
- 
+
 
 int int_random5 = (int)Math.floor(Math.random()*(max5-min5+1)+min5);
- 
 
-   
+
+
       System.out.println(int_random5);
-     
-      
+
+
       String question5= (null);
 if (int_random5 == 9)   {question5 = "9";}else
 if (int_random5 == 8)   {question5 = "8";}else
@@ -226,16 +275,16 @@ System.out.println(question5);
 //--------------------------------------------------------------------------------------------------
 int min6 = -25;
 int max6 =  9;
- 
+
 
 int int_random6 = (int)Math.floor(Math.random()*(max6-min6+1)+min6);
- 
 
-   
+
+
       System.out.println(int_random6);
-     
-      
-      String question5= (null);
+
+
+      String question6= (null);
 if (int_random6 == 9)   {question6 = "9";}else
 if (int_random6 == 8)   {question6 = "8";}else
 if (int_random6 == 7)   {question6 = "7";}else
@@ -275,7 +324,7 @@ if (int_random6 == -25) {question6 = "y";}
 System.out.println(question6);
 //--------------------------------------------------------------------------------------------------
         try {
-            Runtime.getRuntime().exec(new String[]{"cmd", "/c","start chrome https://prnt.sc/"+question3+question4+question6});
+            Runtime.getRuntime().exec(new String[]{"cmd", "/c","start chrome https://prnt.sc/"+question1+question2+question3+question4+question5+question6});
         } catch (Exception e) {
 
         }
@@ -284,4 +333,4 @@ System.out.println(question6);
 
 
 
-        }
+        } 
